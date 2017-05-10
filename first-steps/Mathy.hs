2667f8dbce325708222c0870ceb5267f8a782388
Eric Appelt <eric.appelt@gmail.com>
2017-05-09 22:24:57 -0500

    There -  I memoized it

--- first-steps/Mathy.hs
+++ first-steps/Mathy.hs
@@ -1,9 +1,9 @@
 -- Mathy stuff for simple programs
 module Mathy
 ( fac,
-  fib
+  fib,
+  ffib
 ) where
-
 
 fac :: Integer -> Integer
 fac n
@@ -17,3 +17,22 @@ fib n
     | n == 1 = 1
     | n == 2 = 1
     | otherwise = (fib (n - 1)) + (fib (n - 2))
+
+getIndex :: [Integer] -> Integer -> Integer
+getIndex (x:xs) 0 = x
+getIndex (x:xs) n = getIndex xs (n - 1)
+
+ffib :: Integer -> Integer
+ffib = getIndex (map f (map toInteger [0 ..]))
+    where f 1 = 1
+          f 2 = 2
+          f n = ffib (n-2) + ffib (n-1)
+
+--memoize :: (Integer -> Integer) -> (Integer -> Integer)
+--memoize f = getIndex (map f (map toInteger [0 ..]))
+
+--fix :: (a -> a) -> a
+--fix f = let {x = f x} in x
+
+--ffib :: Integer -> Integer
+--ffib = fix (memoize . fib)
--- first-steps/fib.hs
+++ first-steps/fib.hs
@@ -3,4 +3,4 @@ import qualified Mathy as M
 main = do
     putStrLn "Enter a positive integer:"
     number <- getLine
-    putStrLn ("Fibonacci number " ++ number ++ " is " ++ (show (M.fib (read number::Integer))))
+    putStrLn ("Fibonacci number " ++ number ++ " is " ++ (show (M.ffib (read number::Integer))))
