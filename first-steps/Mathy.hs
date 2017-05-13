b07aacf83763b1b636d76049176ac828a3102eee
Eric Appelt <eric.appelt@gmail.com>
2017-05-12 22:14:50 -0500

    Start playing with binary trees

--- first-steps/Mathy.hs
+++ first-steps/Mathy.hs
@@ -35,3 +35,16 @@ cfib f n = f (n-1) + f (n-2)
 
 ffib :: Integer -> Integer
 ffib = fix (memoize . cfib)
+
+-- Trees are kinda mathy, to be honest (taken from learnyouahaskell)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
